2bd9cdf521eabe12597943d2c7b3b45a2e33a73d
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-01-03 06:49:51 +0700

    Update help.js

--- language/help.js
+++ language/help.js
@@ -457,18 +457,8 @@ Thanks!
 
   *_SEWA BOT_* 
   *≻* Sewa Bot 15k (1 minggu)
-  *≻* Sewa Bot 20k (1 bulan)
-  *≻* Sewa Bot 25k (Permanen)
-
-  *JADI BOT* 
-  *≻* Jadi Bot 15k Tidak Owner (1 minggu)
-  *≻* Jadi Bot + Owner 20k (1 bulan)
-  *≻* Jadi Bot + Owner 25k (Permanen)
-
-  *JASA RUN BOT*
-  *≻* Run Bot 15k (1 minggu)
-  *≻* Run Bot 20k (1 bulan)
-  *≻* Run Bot 25k (Permanen)
+  *≻* Sewa Bot 25k (1 bulan)
+  *≻* Sewa Bot 30k (Permanen) recomented
 
   *Minat? Langsung chat aja ya cuy 🐦*
 
@@ -953,14 +943,7 @@ exports.ownermenu = (prefix) => {
 exports.thanksto = () => {
 	return`
   *THANKS TO*
-  *≻* DikaArdnt
-  *≻* ZeeoneOfc
-  *≻* FatihArridho
-  *≻* Yoga
-  *≻* Riy
-  *≻* Deff
-  *≻* Sanzy
-  *≻* Nekel
-  *≻* Bagas Prdn
+  *≻* Lisabotz-MD🤖🥰🐸
+  *≻* AnggaKz🐸
 `
 }
